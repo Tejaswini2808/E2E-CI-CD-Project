084d7d56771a45675ef07712aae6cd6fea57acd2
Ubuntu <ubuntu@ip-172-31-6-242.ap-southeast-2.compute.internal>
2026-03-28 14:04:20 +0000

    updated req

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ubuntu/Jenkins-Project/venv
+set -gx VIRTUAL_ENV /home/ubuntu/E2E-CI-CD-Project/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
